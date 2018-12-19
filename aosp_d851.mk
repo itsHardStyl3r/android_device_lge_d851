7d255d3fcae59d5b592ad7ea0e6a11bd86a328ec
Aleksander Gencel <johnnylittleplanet@gmail.com>
2018-12-19 23:31:32 +0100

    AOSPExtended for d851

--- aosp_d851.mk
+++ aosp_d851.mk
@@ -22,12 +22,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from d851 device
 $(call inherit-product, device/lge/d851/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSPExtended stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1440
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := d851
-PRODUCT_NAME := lineage_d851
+PRODUCT_NAME := aosp_d851
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-D851
 PRODUCT_MANUFACTURER := LGE
