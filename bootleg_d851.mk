9c62b2646c0277b5bb1133990681c8691bcd1cb3
Aleksander Gencel <johnnylittleplanet@gmail.com>
2019-01-27 11:36:09 +0100

    Bootleggers for d851

--- bootleg_d851.mk
+++ bootleg_d851.mk
@@ -22,12 +22,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from d851 device
 $(call inherit-product, device/lge/d851/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Bootleggers stuff.
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+
+TARGET_PICK_BOOTANIMATION := 9
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := d851
-PRODUCT_NAME := lineage_d851
+PRODUCT_NAME := bootleg_d851
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-D851
 PRODUCT_MANUFACTURER := LGE
